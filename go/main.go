347ea17ae19413f88730261b97ae18f697b2e1e0
NAKATA Keisuke <keisuke.nakata.919@gmail.com>
2023-10-14 22:34:36 +0900

    debug

--- go/main.go
+++ go/main.go
@@ -809,6 +809,8 @@ func getIsuGraph(c echo.Context) error {
 	}
 	date := time.Unix(datetimeInt64, 0).Truncate(time.Hour)
 
+	c.Logger().Debugf("datetimeStr: %v, datetimeInt64: %v, date: %v", datetimeStr, datetimeInt64, date)
+
 	tx, err := db.Beginx()
 	if err != nil {
 		c.Logger().Errorf("db error: %v", err)
@@ -855,6 +857,7 @@ func generateIsuGraphResponse(tx *sqlx.Tx, jiaIsuUUID string, graphDate time.Tim
 		return nil, fmt.Errorf("db error: %v", err)
 	}
 
+	// var lastTruncatedConditionTime time.Time = time.Time{}
 	for rows.Next() {
 		err = rows.StructScan(&condition)
 		if err != nil {
@@ -862,6 +865,9 @@ func generateIsuGraphResponse(tx *sqlx.Tx, jiaIsuUUID string, graphDate time.Tim
 		}
 
 		truncatedConditionTime := condition.Timestamp.Truncate(time.Hour)
+		// if lastTruncatedConditionTime.IsZero() {
+		// 	lastTruncatedConditionTime = truncatedConditionTime
+		// }
 		if truncatedConditionTime != startTimeInThisHour {
 			if len(conditionsInThisHour) > 0 {
 				data, err := calculateGraphDataPoint(conditionsInThisHour)
@@ -946,6 +952,109 @@ func generateIsuGraphResponse(tx *sqlx.Tx, jiaIsuUUID string, graphDate time.Tim
 	}
 
 	return responseList, nil
+
+	// dataPoints := []GraphDataPointWithInfo{}
+	// conditionsInThisHour := []IsuCondition{}
+	// timestampsInThisHour := []int64{}
+	// var startTimeInThisHour time.Time
+	// var condition IsuCondition
+
+	// rows, err := tx.Queryx("SELECT * FROM `isu_condition` WHERE `jia_isu_uuid` = ? ORDER BY `timestamp` ASC", jiaIsuUUID)
+	// if err != nil {
+	// 	return nil, fmt.Errorf("db error: %v", err)
+	// }
+
+	// for rows.Next() {
+	// 	err = rows.StructScan(&condition)
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+
+	// 	truncatedConditionTime := condition.Timestamp.Truncate(time.Hour)
+	// 	if truncatedConditionTime != startTimeInThisHour {
+	// 		if len(conditionsInThisHour) > 0 {
+	// 			data, err := calculateGraphDataPoint(conditionsInThisHour)
+	// 			if err != nil {
+	// 				return nil, err
+	// 			}
+
+	// 			dataPoints = append(dataPoints,
+	// 				GraphDataPointWithInfo{
+	// 					JIAIsuUUID:          jiaIsuUUID,
+	// 					StartAt:             startTimeInThisHour,
+	// 					Data:                data,
+	// 					ConditionTimestamps: timestampsInThisHour})
+	// 		}
+
+	// 		startTimeInThisHour = truncatedConditionTime
+	// 		conditionsInThisHour = []IsuCondition{}
+	// 		timestampsInThisHour = []int64{}
+	// 	}
+	// 	conditionsInThisHour = append(conditionsInThisHour, condition)
+	// 	timestampsInThisHour = append(timestampsInThisHour, condition.Timestamp.Unix())
+	// }
+
+	// if len(conditionsInThisHour) > 0 {
+	// 	data, err := calculateGraphDataPoint(conditionsInThisHour)
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+
+	// 	dataPoints = append(dataPoints,
+	// 		GraphDataPointWithInfo{
+	// 			JIAIsuUUID:          jiaIsuUUID,
+	// 			StartAt:             startTimeInThisHour,
+	// 			Data:                data,
+	// 			ConditionTimestamps: timestampsInThisHour})
+	// }
+
+	// endTime := graphDate.Add(time.Hour * 24)
+	// startIndex := len(dataPoints)
+	// endNextIndex := len(dataPoints)
+	// for i, graph := range dataPoints {
+	// 	if startIndex == len(dataPoints) && !graph.StartAt.Before(graphDate) {
+	// 		startIndex = i
+	// 	}
+	// 	if endNextIndex == len(dataPoints) && graph.StartAt.After(endTime) {
+	// 		endNextIndex = i
+	// 	}
+	// }
+
+	// filteredDataPoints := []GraphDataPointWithInfo{}
+	// if startIndex < endNextIndex {
+	// 	filteredDataPoints = dataPoints[startIndex:endNextIndex]
+	// }
+
+	// responseList := []GraphResponse{}
+	// index := 0
+	// thisTime := graphDate
+
+	// for thisTime.Before(graphDate.Add(time.Hour * 24)) {
+	// 	var data *GraphDataPoint
+	// 	timestamps := []int64{}
+
+	// 	if index < len(filteredDataPoints) {
+	// 		dataWithInfo := filteredDataPoints[index]
+
+	// 		if dataWithInfo.StartAt.Equal(thisTime) {
+	// 			data = &dataWithInfo.Data
+	// 			timestamps = dataWithInfo.ConditionTimestamps
+	// 			index++
+	// 		}
+	// 	}
+
+	// 	resp := GraphResponse{
+	// 		StartAt:             thisTime.Unix(),
+	// 		EndAt:               thisTime.Add(time.Hour).Unix(),
+	// 		Data:                data,
+	// 		ConditionTimestamps: timestamps,
+	// 	}
+	// 	responseList = append(responseList, resp)
+
+	// 	thisTime = thisTime.Add(time.Hour)
+	// }
+
+	// return responseList, nil
 }
 
 // 複数のISUのコンディションからグラフの一つのデータ点を計算
