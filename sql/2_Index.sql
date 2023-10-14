19a83e3f994fba79fcd498d3bcd4204e177cb99c
NAKATA Keisuke <keisuke.nakata.919@gmail.com>
2023-10-14 21:40:59 +0900

    gyaku desita

--- sql/2_Index.sql
+++ sql/2_Index.sql
@@ -1,6 +1,6 @@
 ALTER TABLE isu_condition ADD INDEX jia_isu_uuid_timestamp_desc_idx (jia_isu_uuid, timestamp DESC);
 ALTER TABLE isu_condition ADD COLUMN condition_level CHAR(8) GENERATED ALWAYS AS (CASE
-    WHEN LENGTH(`condition`) = 47 THEN 'info'
+    WHEN LENGTH(`condition`) = 47 THEN 'critical'
     WHEN LENGTH(`condition`) IN (48, 49) THEN 'warning'
-    WHEN LENGTH(`condition`) = 50 THEN 'critical'
+    WHEN LENGTH(`condition`) = 50 THEN 'info'
     END) STORED;
